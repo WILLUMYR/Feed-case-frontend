f8d4e615237cde9df255ea99f6148f3e27e81075
Markus Myrland <markusmyrland@Markuss-MacBook-Pro-2.local>
2020-09-14 17:09:30 +0200

    working

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import './App.css';
 import Filter from '../filter/Filter';
 import Datecard from '../datecard/Datecard';
@@ -8,10 +8,22 @@ import Article01 from '../articles/Article01';
 import Article02 from '../articles/Article02';
 
 function App() {
+  const inputM = useRef(null);
+  const inputT = useRef(null);
+  const [date, setDate] = useState('');
+
+  useEffect(() => {
+    console.log(inputM.getBoundingClientRect())
+  }, [date])
+
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 200) setDate('Monday')
+  })
+
   return (
-    <main className="content">
+    <main className={date === 'Monday'? 'content-M' : 'content'}>
       <Filter />
-      <Datecard date="Tirsdag 12.oktober"/>
+      <Datecard ref={inputT} date="Tirsdag 12.oktober"/>
       <Events time="01:13:18"/>
       <MainArticle />
       <main className="articles">
@@ -23,8 +35,10 @@ function App() {
         <Article01 />
         <Article02 />
       </section>
-
       </main>
+      <Datecard ref={inputM} date="Mandag 11.oktober"/>
+      <MainArticle />
+      <MainArticle />
     </main>
   );
 }
