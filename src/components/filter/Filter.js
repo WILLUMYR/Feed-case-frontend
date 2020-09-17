6f758c355a83bcb22a62d8845b6883badbaf2039
Markus Myrland <markusmyrland@Markuss-MacBook-Pro-2.local>
2020-09-17 11:22:43 +0200

    Finished responsive view for iphone 8

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -5,22 +5,26 @@ const Filter = ({windowWidth}) => {
     if(windowWidth <= 1024) return (
         <nav className="filter">
             <h2>Filter</h2>
-            <button className="filter__button"> Essay </button>
-            <button className="filter__button"> Podcast </button>
-            <button className="filter__button"> Film </button>
-            <button className="filter__button"> Seminar </button>
-            <button className="filter__button"> Lyd </button>
-            <button className="filter__button"> English </button>
+            <div className="filter__buttons">
+                <button className="filter__button"> Essay </button>
+                <button className="filter__button"> Podcast </button>
+                <button className="filter__button"> Film </button>
+                <button className="filter__button"> Seminar </button>
+                <button className="filter__button"> Lyd </button>
+                <button className="filter__button"> English </button>
+            </div>
         </nav>
     );
     return (
         <nav className="filter">
-            <button className="filter__button"> Essay </button>
-            <button className="filter__button"> Podcast </button>
-            <button className="filter__button"> Film </button>
-            <button className="filter__button"> Seminar </button>
-            <button className="filter__button"> Lyd </button>
-            <button className="filter__button"> English </button>
+            <div className="filter__buttons">
+                <button className="filter__button"> Essay </button>
+                <button className="filter__button"> Podcast </button>
+                <button className="filter__button"> Film </button>
+                <button className="filter__button"> Seminar </button>
+                <button className="filter__button"> Lyd </button>
+                <button className="filter__button"> English </button>
+            </div>
         </nav>
     )
 };
